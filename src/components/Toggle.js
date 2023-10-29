7b2081d9bc1e97ba1fd71c104f32846cb4c52ea1
joshua chivile <joshuachivile@gmail.com>
2023-10-29 23:20:28 +0300

    conditional render of color

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -2,8 +2,13 @@ import React, { useState } from "react";
 
 export default function Toggle() {
   const [isOn, setIsOn] = useState(true);
+  const color = isOn ? "red" : "blue";
   const handleToggle = () => {
     setIsOn(!isOn);
   };
-  return <button onClick={handleToggle}>{isOn ? "ON" : "OFF"}</button>;
+  return (
+    <button style={{ background: color }} onClick={handleToggle}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
